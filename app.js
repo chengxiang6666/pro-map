7d1d3c2922cce54e82c52586a31258847d171095
chengxiang6666 <140968173+chengxiang6666@users.noreply.github.com>
2023-07-31 11:09:39 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,48 +6,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
 
-// 使用ArcGIS的地图服务URL
-var serviceUrl = 'https://services.arcgisonline.com/arcgis/rest/services/ChinaBoundary/MapServer';
+// 使用GeoJSON数据加载中国34个省边界
+fetch('China.json')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // 过滤出中国34个省边界数据
+        var chinaProvinces = data.features.filter(function(feature) {
+            return feature.properties.level === 'province' && feature.properties.name !== '台湾省'; // 排除台湾省
+        });
 
-// 加载地图数据并添加过滤器
-L.esri.dynamicMapLayer({
-    url: serviceUrl,
-    opacity: 0.7,
-    layers: [0], // 仅加载第一个图层，即中国省级边界图层
-    where: "ID IN (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34)" // 仅显示ID为1到34的要素
-}).addTo(map);
+        // 创建GeoJSON图层并添加到地图上
+        var provincesLayer = L.geoJSON(chinaProvinces).addTo(map);
 
-// 创建Popup对象
-var popup = L.popup();
-
-// 点击事件处理函数
-function onMapClick(e) {
-    // 获取点击位置的地理坐标
-    var latlng = e.latlng;
-
-    // 查询点击位置的要素信息
-    L.esri.query({
-        url: serviceUrl + '/0' // 要素服务URL，根据实际情况修改
-    }).nearby(latlng, 10).run(function(error, featureCollection) {
-        if (error) return;
-
-        // 如果找到了要素
-        if (featureCollection.features.length > 0) {
-            var feature = featureCollection.features[0];
-
-            // 获取要素属性信息
-            var attributes = feature.properties;
-
-            // 组装要素信息文本
-            var info = "这是" + attributes.NAME + "省";
-
-            // 设置弹出窗口内容
-            popup.setLatLng(latlng)
-                .setContent(info)
-                .openOn(map);
-        }
+        // 调整地图视图以适应中国34个省的边界
+        map.fitBounds(provincesLayer.getBounds());
     });
-}
-
-// 监听地图点击事件
-map.on('click', onMapClick);
