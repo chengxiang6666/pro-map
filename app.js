c86569d14ab6418212d25e11e7ccdc082ae149aa
chengxiang6666 <140968173+chengxiang6666@users.noreply.github.com>
2023-07-31 10:50:43 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -6,38 +6,46 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
 }).addTo(map);
 
-// 绘制中国省级地图
-var geojsonLayer = L.geoJSON(chinaData, {
-    style: function (feature) {
-        return {
-            weight: 1,
-            opacity: 1,
-            color: 'white',
-            fillOpacity: 0.7,
-            fillColor: getRandomColor()
-        };
-    },
-    onEachFeature: function (feature, layer) {
-        layer.on('click', function (e) {
-            showInfoBox(e.target);
-        });
-    }
+// 使用ArcGIS的地图服务URL
+var serviceUrl = 'https://services.arcgisonline.com/arcgis/rest/services/ChinaBoundary/MapServer';
+
+// 加载地图数据
+L.esri.dynamicMapLayer({
+    url: serviceUrl,
+    opacity: 0.7
 }).addTo(map);
 
-// 生成随机颜色
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+// 创建Popup对象
+var popup = L.popup();
+
+// 点击事件处理函数
+function onMapClick(e) {
+    // 获取点击位置的地理坐标
+    var latlng = e.latlng;
+
+    // 查询点击位置的要素信息
+    L.esri.query({
+        url: serviceUrl + '/0' // 要素服务URL，根据实际情况修改
+    }).nearby(latlng, 10).run(function(error, featureCollection) {
+        if (error) return;
+
+        // 如果找到了要素
+        if (featureCollection.features.length > 0) {
+            var feature = featureCollection.features[0];
+
+            // 获取要素属性信息
+            var attributes = feature.properties;
+
+            // 组装要素信息文本
+            var info = "这是" + attributes.NAME + "省";
+
+            // 设置弹出窗口内容
+            popup.setLatLng(latlng)
+                .setContent(info)
+                .openOn(map);
+        }
+    });
 }
 
-// 显示信息框
-function showInfoBox(layer) {
-    var province = layer.feature.properties.name;
-    var info = "这是" + province + "省";
-    alert(info);
-}
-// JavaScript Document
+// 监听地图点击事件
+map.on('click', onMapClick);
